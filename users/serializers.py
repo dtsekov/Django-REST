38ce21ebc23150c3c5c45958fa9c27e633d29130
dimitardanielovtsekov <d.tsekov@alumnos.upm.es>
2025-07-04 22:53:56 +0200

    Arreglos en logica de login

--- users/serializers.py
+++ users/serializers.py
@@ -6,17 +6,21 @@ from rest_framework import serializers
 Usuario = get_user_model()
 
 class UsuarioSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Usuario
         fields = [
             'id', 'email', 'nombre', 'rol_actual',
             'phone_number', 'activo', 'fecha_activacion',
-            'created_at', 'updated_at'
+            'created_at', 'updated_at', 'password'
         ]
         # No permitir que rol_actual sea asignado en creación
         read_only_fields = ['id', 'rol_actual', 'activo', 'fecha_activacion', 'created_at', 'updated_at']
 
     def create(self, validated_data):
+        validated_data['rol_actual'] = 'anonimo'
         password = validated_data.pop('password', None)
         usuario = Usuario.objects.create_user(
             email=validated_data.get('email'),
@@ -25,15 +29,24 @@ class UsuarioSerializer(serializers.ModelSerializer):
             phone_number=validated_data.get('phone_number')
         )
         return usuario
+    
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+        if 'rol_actual' in validated_data and request.user.rol_actual != 'coordinador':
+            validated_data.pop('rol_actual')  # Solo coordinador puede cambiarlo
+        return super().update(instance, validated_data)
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+
     username_field = Usuario.USERNAME_FIELD
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Rename 'username' field to 'email'
-        if 'username' in self.fields:
-            self.fields['email'] = self.fields.pop('username')
+        self.fields['email'] = serializers.EmailField()
+        self.fields['password'] = serializers.CharField()
+
+   
 
     @classmethod
     def get_token(cls, user):
